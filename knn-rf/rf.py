d9a941002fab4a6b0e4a9198f708ae15565f7247
Büşra Marşan <46198726+iambusra@users.noreply.github.com>
2023-06-03 21:31:07 +0300

    Add KNN and RF code

---
+++ knn-rf/rf.py
@@ -0,0 +1,98 @@
+import json
+import numpy as np
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+from sklearn.feature_extraction.text import CountVectorizer
+from tqdm import tqdm
+
+# Load data from file
+with open('data_train.json', 'r') as f:
+    train_data = json.load(f)
+with open('data_dev.json', 'r') as f:
+    dev_data = json.load(f)
+with open('data_test.json', 'r') as f:
+    test_data = json.load(f)
+
+train_data = train_data + dev_data
+
+# Get input data sets
+train_premises = [example['premise'] for example in train_data]
+train_hypotheses = [example['hypothesis'] for example in train_data]
+train_labels = [example['gold_label'] for example in train_data]
+
+test_premises = [example['premise'] for example in test_data]
+test_hypotheses = [example['hypothesis'] for example in test_data]
+test_labels = [example['gold_label'] for example in test_data]
+
+# Vectorize premise and hypothesis texts using a pre-trained word embedding model
+vectorizer = CountVectorizer()
+
+vectorizer.fit(train_premises)
+vectorizer.fit(train_hypotheses)
+
+vectorizer.fit(test_premises)
+vectorizer.fit(test_hypotheses)
+
+train_X = np.concatenate(
+    [vectorizer.transform(train_premises).toarray(),
+     vectorizer.transform(train_hypotheses).toarray()], axis=1)
+test_X = np.concatenate(
+    [vectorizer.transform(test_premises).toarray(),
+     vectorizer.transform(test_hypotheses).toarray()], axis=1)
+
+train_y = np.array(train_labels)
+
+n_estimators = 103 # number of trees in the forest
+max_depth = None # maximum depth of each tree
+random_state = 42 # random seed for reproducibility
+class_weight = 'balanced_subsample' # balance classes for each tree
+min_samples_leaf = 5
+weigth_1 = np.count_nonzero(train_y == 1)/len(train_y)
+weigth_0 = 1-weigth_1
+class_weight = {0:weigth_0, 1:weigth_1}
+
+print("weigths", weigth_1, weigth_0)
+rf = RandomForestClassifier(
+    n_estimators=n_estimators, 
+    max_depth=max_depth, 
+    random_state=random_state,
+    class_weight=class_weight,
+    min_samples_leaf=min_samples_leaf)
+
+#rf.fit(train_X, train_y)
+for i in tqdm(range(len(train_X))):
+    rf.fit([train_X[i]], [train_y[i]])
+
+# Make predictions on the test set
+pred_y = rf.predict(test_X)
+
+# Evaluate the accuracy of the model
+accuracy = accuracy_score(test_labels, pred_y)
+print("Accuracy:", accuracy)
+np.save('pred_y_skrf.npy', pred_y)
+
+# Some needed metrics
+pred_0 = 0
+pred_1 = 0
+success = 0
+fail = 0
+for i, actual in enumerate(test_labels):
+    prediction = pred_y[i]
+    
+    if prediction == 0:
+        pred_0 += 1
+    else:
+        pred_1 += 1
+
+    if actual == prediction:
+        success += 1
+    else:
+        fail += 1
+
+print("predited 0s:", pred_0, "rate:", pred_0/len(test_labels))
+print("predited 1s:", pred_1, "rate:", pred_1/len(test_labels))
+
+print()
+print("success rate:", success/len(test_labels))
+
+
